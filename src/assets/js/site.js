22859abd388bd42e10580248f29a6a2e23d1d35c
skql1715 <skql1715@naver.com>
2025-03-17 16:50:48 +0900

    🤩스킬 완료

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,3 +1,19 @@
 export function site() {
     console.log(site)
+
+    gsap.to(".square", {
+        rotation: -90,    // 도 회전
+        duration: 1,      // 초 동안
+        repeat: -1,       // 무한 반복
+        yoyo: true,      // 애니메이션이 끝날 때마다 반대로 돌아가도록
+        ease: "linear"    // 일정한 속도로 회전
+    });
+    gsap.to(".triangle", {
+      rotation: 360,    // 도 회전
+      duration: 5,      // 초 동안
+      repeat: -1,       // 무한 반복
+      yoyo: true,      // 애니메이션이 끝날 때마다 반대로 돌아가도록
+      ease: "linear"    // 일정한 속도로 회전
+    });
+    
 }
